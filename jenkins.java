deaf3f52089474ec3cff1e663ca2a540e17c40bb
NIHAR J PANCHAL <nihar.p>
2026-08-21 08:39:09 +0530

    Initial Commit

---
+++ jenkins.java
@@ -0,0 +1,7 @@
+// HelloWorld.java
+public class jenkins {
+    public static void main(String[] args) {
+        // Print message to the console
+        System.out.println("Hello, World!");
+    }
+}
